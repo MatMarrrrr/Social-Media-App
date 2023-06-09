1c271eab66877424a328a6aafde9f124012805b4
MatMarrr <mateuszrmarek@gmail.com>
2023-06-10 01:02:10 +0200

    Start adding liking functionality

--- src/pages/main/post.tsx
+++ src/pages/main/post.tsx
@@ -1,10 +1,38 @@
 import { Post as IPost } from "./main";
+import {
+  addDoc,
+  getDocs,
+  collection,
+  query,
+  where,
+  getDoc,
+} from "firebase/firestore";
+import { auth, db } from "../../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useEffect } from "react";
 interface Props {
   post: IPost;
 }
 
 function Post(props: Props) {
   const { post } = props;
+  const [user] = useAuthState(auth);
+
+  const likesRef = collection(db, "likes");
+  const likesDoc = query(likesRef, where("postId", "==", post.id));
+
+  const getLikes = async () => {
+    const data = await getDocs(likesDoc);
+    console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  };
+  const addLike = async () => {
+    await addDoc(likesRef, { user: user?.uid, postId: post.id });
+  };
+
+  useEffect(() => {
+    getLikes();
+  }, []);
+
   return (
     <div>
       <div className="title">
@@ -15,7 +43,8 @@ function Post(props: Props) {
       </div>
       <div className="postFooter">
         <p>@{post.username}</p>
-        <button>&#128077;</button>
+        <button onClick={addLike}>&#128077;</button>
+        <p>Likes: {}</p>
       </div>
     </div>
   );
