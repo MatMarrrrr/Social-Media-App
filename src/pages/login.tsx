f2395652ca556d5aaf9e69d6db355de42bbe7ccf
MatMarrr <mateuszrmarek@gmail.com>
2023-06-11 00:37:15 +0200

    Add redirect from login page when user is logged in

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,19 +1,28 @@
 import "../style/Login.css";
+import { useEffect } from "react";
 import { auth, provider } from "../config/firebase";
 import { signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 function Login() {
   const navigate = useNavigate();
+  const [user] = useAuthState(auth);
+
   const signInWithGoogle = async () => {
     try {
-      const result = await signInWithPopup(auth, provider);
-      console.log(result);
+      await signInWithPopup(auth, provider);
       navigate("/");
     } catch (error) {
       console.log(error);
     }
   };
+
+  useEffect(() => {
+    if (user) {
+      navigate("/");
+    }
+  }, [user]);
 
   return (
     <div className="Login">
