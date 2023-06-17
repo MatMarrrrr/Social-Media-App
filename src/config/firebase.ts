94cbf47aef68d7ebb3c77b54bc9c3f321a01efbb
MatMarrr <mateuszrmarek@gmail.com>
2023-06-18 00:24:08 +0200

    Update live version

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,18 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCU9NpEdG4CdHFvr8mfgmu7xmJBRv-2AmY",
-  authDomain: "social-media-app-ed3c0.firebaseapp.com",
-  projectId: "social-media-app-ed3c0",
-  storageBucket: "social-media-app-ed3c0.appspot.com",
-  messagingSenderId: "629056579250",
-  appId: "1:629056579250:web:986bf1d10a3ac567960bab",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
